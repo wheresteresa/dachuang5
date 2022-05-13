27d7ccbf67521eb793c4c1941fe20bd6a2d62857
wheresteresa <64266886+wheresteresa@users.noreply.github.com>
2022-05-13 15:36:00 +0800

    Add files via upload

---
+++ ryh.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+  int a,b,c;
+  printf("请输入两个整数\n");
+  printf("整数a:");
+  scanf("%d",&a);
+  printf("整数b:");
+  scanf("%d",&b);
+  c=(a*100)/b;
+  printf("a的值是b的%d%%\n",c);
+  a[2] = 0;
+  a[-1] = 5;
+  return 0;
+}
